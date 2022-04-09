d8bb80859bc90c727e58779ed98fe9658c1a9050
David <david.turley@live.nl>
2022-04-09 21:20:30 +0200

    Added char stats

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-let checkboxDiv = document.getElementById("checkboxes");
+///////////////////////// Checkboxes /////////////////////////
+const checkboxDiv = document.getElementById("checkboxDiv");
 
-// Add 
-CreateNewCheckboxElement("Point Blank Shot", "pointBlankShot", checkboxDiv);
-CreateNewCheckboxElement("Rapid Shot", "rapidShot", checkboxDiv);
-CreateNewCheckboxElement("Manyshot", "manyshot", checkboxDiv);
-CreateNewCheckboxElement("Weapon Focus", "weaponFocus", checkboxDiv);
-CreateNewCheckboxElement("Rangers Focus", "rangersFocus", checkboxDiv);
+const Checkboxes = [
+    CreateInputElement("Point Blank Shot", "pointBlankShot", "modifier", "checkbox", checkboxDiv),
+    CreateInputElement("Rapid Shot", "rapidShot", "modifier", "checkbox", checkboxDiv),
+    CreateInputElement("Manyshot", "manyshot", "modifier", "checkbox", checkboxDiv),
+    CreateInputElement("Weapon Focus", "weaponFocus", "modifier", "checkbox", checkboxDiv),
+    CreateInputElement("Rangers Focus", "rangersFocus", "modifier", "checkbox", checkboxDiv)
+];
+
+///////////////////////// Stats /////////////////////////
+const statDiv = document.getElementById("statDiv");
+
+const Stats = [
+    CreateInputElement("Strength", "str", "statNumInput", "number", statDiv),
+    CreateInputElement("Dexterity", "dex", "statNumInput", "number", statDiv),
+    CreateInputElement("Constitution", "con", "statNumInput", "number", statDiv),
+    CreateInputElement("Inteligence", "int", "statNumInput", "number", statDiv),
+    CreateInputElement("Wisdom", "wis", "statNumInput", "number", statDiv),
+    CreateInputElement("Charisma", "cha", "statNumInput", "number", statDiv)
+];
+
+Stats[0].value = 16;
+Stats[1].value = 19;
+Stats[2].value = 8;
+Stats[3].value = 10;
+Stats[4].value = 12;
+Stats[5].value = 8;
+
+///////////////////////// Attack /////////////////////////
 
 
+///////////////////////// General /////////////////////////
 
-
-
-function CreateNewCheckboxElement(text, id, elementToAppendTo){
+function CreateInputElement(text, id, inputClass, inputType, elementToAppendTo){
     const fragment = document.createDocumentFragment();
     const label = document.createElement("label");
+    
     const labelText = document.createTextNode(text);
     label.setAttribute("for", id);
+    
     const input = document.createElement('input');
-    input.setAttribute('type', 'checkbox');
+    input.setAttribute('type', inputType);
+    input.setAttribute('class', inputClass);
     input.name = id;
     input.id = id;
 
@@ -27,4 +52,6 @@ function CreateNewCheckboxElement(text, id, elementToAppendTo){
 
     elementToAppendTo.appendChild(fragment);
     elementToAppendTo.appendChild(document.createElement("br"));
+
+    return input;
 }
